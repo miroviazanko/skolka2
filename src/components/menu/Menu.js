a903f553dfd33d0ed4cac95ec8c0d5297f50d957
miroviazanko <miro.viazanko@gmail.com>
2020-10-30 09:59:48 +0100

    zelene listky 2. verzia

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -3,11 +3,11 @@ import React from "react";
 
 import styles from './Menu.module.scss';
 
-import leaf1 from "../../assets/menu-clip/leafes/leaf1.png";
-import leaf2 from "../../assets/menu-clip/leafes/leaf2.png";
-import leaf3 from "../../assets/menu-clip/leafes/leaf3.png";
-import leaf4 from "../../assets/menu-clip/leafes/leaf4.png";
-import leaf5 from "../../assets/menu-clip/leafes/leaf5.png";
+import leaf1 from "../../assets/menu-clip/leafes/leafn1.png";
+import leaf2 from "../../assets/menu-clip/leafes/leafn2.png";
+import leaf3 from "../../assets/menu-clip/leafes/leafn3.png";
+import leaf4 from "../../assets/menu-clip/leafes/leafn4.png";
+import leaf5 from "../../assets/menu-clip/leafes/leafn5.png";
 
 
 
@@ -22,13 +22,17 @@ export default function Menu(props) {
         <div className={styles.menuContainer}>
             <ul className={styles.list}>
                 {menuList.map( (m,i) => (
-                    <li className={styles.menuLi}
-                        key={i}>
-                        {m.name}
-                        <img src={  leafs[i] }
-                             alt="menu lístok"
-                             width="50"/>
-                    </li>
+                    <div className={styles.wrapper}>
+
+                            <li className={styles.menuLi}
+                                key={i}>
+                                {m.name}
+                                <img src={  leafs[i] }
+                                    alt="menu lístok"
+                                    width="50"/>
+                            </li>
+
+                    </div>
                 )
                 )}
             </ul>
